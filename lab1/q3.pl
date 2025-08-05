24b1b43caa8915ebada165558e869f604d954264
ThuraAung1601 <thuraaung.ai.mdy@gmail.com>
2025-08-05 15:35:25 +0700

    added solution for q3

--- lab1/q3.pl
+++ lab1/q3.pl
@@ -8,7 +8,26 @@ course(ai, 3, se).
 course(ml, 3, se).
 
 completed(john, dataStructure_and_algorithm).
+completed(mike, dataStructure_and_algorithm).
 completed(tom, probability).
 
 requires(ai_programming, dataStructure_and_algorithm).
 requires(ml, probability).
+
+% rules
+can_enroll(Student, Course) :-
+    student(Student, Y, P),
+    course(Course, Y, P),
+    requires(Course, X), 
+    completed(Student, X).
+
+/*
+can_enroll(Student, Course) :-
+    requires(Course, X), 
+    completed(Student, X).
+*/
+
+possible_classmates(StudentA, StudentB, Course) :-
+    can_enroll(StudentA, Course),
+    can_enroll(StudentB, Course),
+    StudentA \= StudentB.
